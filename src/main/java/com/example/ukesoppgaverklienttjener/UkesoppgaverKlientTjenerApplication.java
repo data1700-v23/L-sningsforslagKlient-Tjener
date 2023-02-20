1ca14bab9868c8ca322ce3ce5ad6a9e8d42a8a57
henriklarsen28 <henrik.hl@hotmail.com>
2023-02-20 21:33:38 +0100

    Initial commit

---
+++ src/main/java/com/example/ukesoppgaverklienttjener/UkesoppgaverKlientTjenerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ukesoppgaverklienttjener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkesoppgaverKlientTjenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UkesoppgaverKlientTjenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ukesoppgaverklienttjener/UkesoppgaverKlientTjenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukesoppgaverklienttjener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkesoppgaverKlientTjenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
